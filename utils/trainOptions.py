da54e25402dc809fe9e5e21d7e37438553429a71
Dra-kt <dra_kt@outlook.com>
2022-11-26 17:26:50 +0800

    修正选项名错误

--- utils/trainOptions.py
+++ utils/trainOptions.py
@@ -38,7 +38,7 @@ class TrainOptions:
         self.parser.add_argument('--patchs', default=0, action='count')
         self.parser.add_argument('--disable-patchs-eval', default=0, action='count')
         self.parser.add_argument('--reset-counter', default=0, action='count')
-        self.parser.add_argument('--diable-img-record', default=0, action='count')
+        self.parser.add_argument('--disable-img-record', default=0, action='count')
 
     def getOpts(self):
         parse = self.parser.parse_args()
