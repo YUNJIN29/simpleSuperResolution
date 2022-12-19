be44d2ce26c31d8fb9e264f0aaa086d92af215ae
Dra-kt <dra_kt@outlook.com>
2022-12-19 16:26:19 +0800

    移除已弃用代码

--- Module.py
+++ Module.py
@@ -23,32 +23,3 @@ class SRCNN(nn.Module):
     def forward(self, x):
         # x = self.pad(x)
         return self.sequential(x)
-
-
-class Covgg7(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.act_fn = nn.LeakyReLU(0.1, inplace=False)
-        self.offset = 7
-        self.pad = nn.ZeroPad2d(self.offset)
-        m = [nn.Conv2d(3, 16, 3, 1, 0),
-             self.act_fn,
-             nn.Conv2d(16, 32, 3, 1, 0),
-             self.act_fn,
-             nn.Conv2d(32, 64, 3, 1, 0),
-             self.act_fn,
-             nn.Conv2d(64, 128, 3, 1, 0),
-             self.act_fn,
-             nn.Conv2d(128, 128, 3, 1, 0),
-             self.act_fn,
-             nn.Conv2d(128, 256, 3, 1, 0),
-             self.act_fn,
-             # in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=
-             nn.ConvTranspose2d(256, 3, kernel_size=4, stride=2, padding=3, bias=False),
-             ]
-        self.Sequential = nn.Sequential(*m)
-
-    def forward(self, x):
-        x = self.pad(x)
-        return self.Sequential.forward(x)
-
